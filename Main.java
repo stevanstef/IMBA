8220f83cd6b04fe1670aa62ee4b5d289b39bcf1a
NikolasRang <Nikolas.Rangoussis@Student.tdsb.on.ca>
2024-05-15 13:14:45 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 class Start {
     public static void main(String[] args) {
         System.out.println("Branching out!");
+        System.out.println("Ohio rizz, you skibidi french guy");
     }
 }
