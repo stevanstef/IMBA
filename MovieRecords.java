3763d8f45572bfb35e71698a4b1dfde5be018386
MattLP24 <matthew.jlpires@gmail.com>
2024-05-22 15:17:36 -0400

    Add files via upload

---
+++ MovieRecords.java
@@ -0,0 +1,24 @@
+//Filename: MovieRecords
+//Author: Matthew Luong-Pires
+//Date: May 22
+//
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+class MovieRecords
+{
+
+   public Records() {
+      // Fixes a warning (warning: use of default constructor, which does not provide a comment)
+      }    
+   
+      /** Gets the records from the input data array.
+       * @param data The array containing the records.
+       * @return A 2D array with separated records.*/
+    
+      public String[][] getRecords(String data[]) {
+      }
+
+   
+}
