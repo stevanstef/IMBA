80f26fa7cbf1050f789faca7cb5d0729ea90e3a2
stevanstef <stevan.stefanovic321@gmail.com>
2024-05-23 13:56:58 -0400

    Add files via upload

--- IMBA.java
+++ IMBA.java
@@ -2,47 +2,111 @@
 //Date: May 21st 2024
 
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.*;
-import java.io.*;
+import javax.swing.border.Border;
 
-public class IMBA extends Frame {
+public class IMBA extends JFrame {
 
-      //Declare variables and constants
-		private JButton btn1;
-      static String movieFile = "movieList.txt";      //movieFile = "movieList.txt"
-      final int MAX = 40;                        //Set MAX number opf records
-      String[][] movieArray = new String[40][40]; 
-      /*ReadData rd = new ReadData();
-      Records raRe = new Records();                
-		Records moRe = new Records();
-      Sorter s = new Sorter();
-      SearchInput si = new SearchInput();
-		DeleteRating dr = new DeleteRating();
-		KeyInput ki = new KeyInput();
-		AddRating nr = new AddRating();*/
-		
-		public void paint(Graphics g) {
-         Toolkit tk = Toolkit.getDefaultToolkit();
-         Image img = tk.getImage("Background.png");
-         g.drawImage(img, 0, 30, this);     
-    }//end paint
-      
-      public IMBA() { // constructor lo prepare window size and menubar
-			btn1 = new JButton("Enter");  
-			add(btn1);			
-			setVisible(true);
-			setLayout(new FlowLayout(FlowLayout.CENTER, 5, 440));
-			setTitle("IMBA");
-			resize(960, 540);
-       	button.addActionListener(new ActionListener(){
+    // Declare variables and constants
+    private JButton enter, search, sort, select;
+    static String movieFile = "movieList.txt"; // movieFile = "movieList.txt"
+    final int MAX = 40; // Set MAX number of records
+    String[] rows = new String[MAX];
+	 String info [][] = new String[MAX][6];
+	 ReadData rd = new ReadData();
+    Records re = new Records(); 
+
+    public IMBA() { // constructor to prepare window size and menubar
+      BackgroundPanel bgPanel = new BackgroundPanel("background.png");
+      setContentPane(bgPanel);
+      setTitle("IMBA");
+      setSize(960, 540); // Window size
+      setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Properly close the application
+      buttons();
+      setVisible(true); // Make the JFrame visible
+    }
+	 
+    private void libraryRun(){
+        enter.setVisible(false);
+        select.setVisible(true);
+        sort.setVisible(true);
+        search.setVisible(true);
+		  
+		  JPanel lib = new JPanel();
+	 	  GridBagConstraints gbc = new GridBagConstraints();
+	 	  gbc.gridx = 1;
+    	  gbc.gridy = 0;
+    	  gbc.insets = new Insets(200, 200, 200, 200);
+	 	  add(lib, gbc);
+	 	  lib.setSize(600,400);
+	     lib.setVisible(true);
+		  rows = rd.readFile(movieFile, 30);
+        info = re.getRecords(rows, lib);
+   }
+
+    private void buttons() {
+        setLayout(new GridBagLayout());
+        enter = new JButton("Enter");
+        enter.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                // Insert code here
+                // Action to be performed when the button is clicked
+                libraryRun();
             }
         });
-      }// end constructor
-		
-   	public static void main(String str[]){
-         	new IMBA();// Instantiates LTS class
-         	} //end main
-}//end class   
+        enter.setVisible(true);
 
+        select = new JButton("Select");
+        select.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                // Action to be performed when the button is clicked
+                libraryRun();
+            }
+        });
+        select.setVisible(false);
+
+        search = new JButton("Search");
+        search.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                // Action to be performed when the button is clicked
+                libraryRun();
+            }
+        });
+        search.setVisible(false);
+
+        sort = new JButton("Sort");
+        sort.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                // Action to be performed when the button is clicked
+                libraryRun();
+            }
+        });
+        sort.setVisible(false);
+
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.gridx = 1;
+        gbc.gridy = 0;
+        gbc.insets = new Insets(400, 1, 1, 1);
+        add(enter, gbc);
+
+        gbc.gridx = 1;
+        gbc.gridy = 0;
+        gbc.insets = new Insets(400, 1, 1, 400);
+        add(select, gbc);
+
+        gbc.gridx = 1;
+        gbc.gridy = 0;
+        gbc.insets = new Insets(1, 1, 430, 800);
+        add(search, gbc);
+
+        gbc.gridx = 1;
+        gbc.gridy = 0;
+        gbc.insets = new Insets(1, 1, 430, 650);
+        add(sort, gbc);
+    } // end makeMenus
+
+    public static void main(String[] args) {
+        new IMBA(); // Instantiates IMBA class
+    } // end main
+} // end class
